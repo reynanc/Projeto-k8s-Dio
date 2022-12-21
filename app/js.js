33eb94752dd8f608f1fc13ecfc3cad999f264ea1
José Reynan <reynan.cavalcanti@gmail.com>
2022-12-21 13:51:41 -0300

    Projeto DIO

---
+++ app/js.js
@@ -0,0 +1,18 @@
+$("#btn_gravar").on("click", function() {
+    
+    var txt_nome = $("#nome").val();
+    var txt_mensagem = $("#mensagem").val(); 
+
+    $.ajax({
+        url: "",
+        type: "post",
+        data: {nome: txt_nome, mensagem: txt_mensagem},
+        beforeSend: function() {
+            $("#resposta").html("Enviando......");
+        }
+    }).done(function(e) {
+        $("#resposta").html("Dados salvos......");
+    })
+
+})
+
